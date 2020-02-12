d9260bc050158f034dbe936792c2c49a753053d8
James <admin@James.local>
2020-02-12 11:13:03 +0300

    initial commit

---
+++ ImageFilters/ImageFilters/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImageFilters
+//
+//  Created by James on 2/12/20.
+//  Copyright © 2020 James. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
