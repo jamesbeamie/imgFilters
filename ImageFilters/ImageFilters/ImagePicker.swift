105b53dab82ce711e759d6474365162fd5d2adb4
James <admin@James.local>
2020-02-13 16:08:20 +0300

    image picker control and coordinators

--- ImageFilters/ImageFilters/ImagePicker.swift
+++ ImageFilters/ImageFilters/ImagePicker.swift
@@ -9,8 +9,29 @@
 import SwiftUI
 // Creating a swiftUI view, ImagePicker
 struct ImagePicker: UIViewControllerRepresentable {
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker){
+            self.parent = parent
+        }
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.originalImage] as? UIImage{
+                parent.image = uiImage
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+    func makeCoordinator() -> Coordinator{
+        Coordinator(self)
+    }
+    
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+    
     func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
         let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
         return picker
     }
     
